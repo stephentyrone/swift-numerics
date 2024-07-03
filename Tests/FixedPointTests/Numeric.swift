e8c8e2d515937e7183643d74d11e1ccac25d6728
Stephen Canon <scanon@apple.com>
2024-07-03 10:10:20 -0400

    WIP

--- Tests/FixedPointTests/Numeric.swift
+++ Tests/FixedPointTests/Numeric.swift
@@ -13,7 +13,7 @@ import FixedPointModule
 import XCTest
 
 func referenceMultiplication(_ a: Int8Q3, _ b: Int8Q3) -> Int8Q3? {
-  Int8Q3(exactly: Double(a)*Double(b), rounding: .down)
+  Int8Q3(exactly: Double(a)*Double(b), rounding: .toNearestOrUp)
 }
 
 func referenceDivision(_ a: Int8Q3, _ b: Int8Q3) -> Int8Q3? {
@@ -24,10 +24,14 @@ final class NumericTests: XCTestCase {
   func testMultiplicationInt8Q3() {
     for a in Int8.min ... Int8.max {
       let qa = Int8Q3(bitPattern: a)
+      XCTAssertEqual(qa.magnitude.bitPattern, a.magnitude)
       for b in Int8.min ... Int8.max {
         let qb = Int8Q3(bitPattern: b)
         if let qr = referenceMultiplication(qa, qb) {
-          XCTAssertEqual(qa*qb, qr)
+          let qp = qa*qb
+          if qp != qr {
+            XCTFail("\(qa)*\(qb) was \(qp), expected \(qr) (\(Double(qa)*Double(qb)))")
+          }
         }
         if let qq = referenceDivision(qa, qb) {
           XCTAssertEqual(qa/qb, qq)
