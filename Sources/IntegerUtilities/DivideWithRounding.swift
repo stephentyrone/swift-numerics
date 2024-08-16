c5d5d6e958796a99903747fb99c73ef483f233e7
Stephen Canon <scanon@apple.com>
2024-08-16 15:33:39 -0400

    Simplify logic in divided(by:rounding:)

--- Sources/IntegerUtilities/DivideWithRounding.swift
+++ Sources/IntegerUtilities/DivideWithRounding.swift
@@ -39,82 +39,58 @@ extension BinaryInteger {
   ) -> Self {
     // "Normal divsion" rounds toward zero, so we get self = q*other + r
     // with |r| < |other| and r matching the sign of self.
-    let q = self / other
-    let r = self - q*other
+    let (q,r) = self.quotientAndRemainder(dividingBy: other)
+    
     // In every rounding mode, the result is the same when the result is
     // exact.
     if r == 0 { return q }
+    
     // From this point forward, we can assume r != 0.
     //
     // To get the quotient and remainder rounded as directed by rule, we
     // will adjust q and r. Note that the quotient is either q-1 or q (if
     // q is negative) or q or q+1 (if q is positive), because q has been
     // rounded toward zero.
-    //
-    // If we subtract 1 from q, we add other to r to compensate, because:
-    //
-    //   self = q*other + r
-    //        = (q-1)*other + (r+other)
-    //
-    // Similarly, if we add 1 to q, we subtract other from r to compensate.
+    
+    let qNeg: Self = other.signum() != self.signum() ? 1 : 0
+    let qAway = q + (1 - 2*qNeg)
+    
     switch rule {
     case .down:
-      // For rounding down, we want to have r match the sign of other
-      // rather than self; this means that if the signs of r and other
-      // disagree, we have to adjust q downward and r to match.
-      if other.signum() != r.signum() { return q-1 }
-      return q
+      return q - qNeg
       
     case .up:
-      // For rounding up, we want to have r have the opposite sign of
-      // other; if not, we adjust q upward and r to match.
-      if other.signum() == r.signum() { return q+1 }
-      return q
+      return q + (1 - qNeg)
       
     case .towardZero:
-      // This is exactly what the `/` operator did for us.
       return q
       
     case .awayFromZero:
-      break
+      return qAway
       
     case .toNearestOrDown:
-      if r.magnitude > other.magnitude.shifted(rightBy: 1, rounding: .down) ||
-          2*r.magnitude == other.magnitude && other.signum() != r.signum() {
-        break
-      }
-      return q
+      let threshold = (other.magnitude - qNeg.magnitude) >> 1
+      return r.magnitude <= threshold ? q : qAway
       
     case .toNearestOrUp:
-      if r.magnitude > other.magnitude.shifted(rightBy: 1, rounding: .down) ||
-          2*r.magnitude == other.magnitude && other.signum() == r.signum() {
-        break
-      }
-      return q
+      let threshold = (other.magnitude - (1 - qNeg.magnitude)) >> 1
+      return r.magnitude <= threshold ? q : qAway
       
     case .toNearestOrZero:
-      if r.magnitude <= other.magnitude.shifted(rightBy: 1, rounding: .down) {
-        return q
-      }
-      // Otherwise, round q away from zero.
+      let threshold = other.magnitude >> 1
+      return r.magnitude <= threshold ? q : qAway
       
     case .toNearestOrAway:
-      if r.magnitude < other.magnitude.shifted(rightBy: 1, rounding: .up) {
-        return q
-      }
+      let threshold = other.magnitude.shifted(rightBy: 1, rounding: .up)
+      return r.magnitude < threshold ? q : qAway
       
     case .toNearestOrEven:
-      // First guarantee that |r| <= |other/2|; if not we have to round away
-      // instead, so break to do that.
-      if r.magnitude > other.magnitude.shifted(rightBy: 1, rounding: .down) ||
-         2*r.magnitude == other.magnitude && !q.isMultiple(of: 2) {
-        break
-      }
-      return q
+      let threshold = (other.magnitude - (q.magnitude & 1)) >> 1
+      return r.magnitude <= threshold ? q : qAway
       
     case .toOdd:
       // If q is already odd, we have the correct result.
-      if q._lowWord & 1 == 1 { return q }
+      return q._lowWord & 1 == 1 ? q : qAway
       
     case .stochastically:
       let bmag = other.magnitude
@@ -130,14 +106,11 @@ extension BinaryInteger {
         rhi = UInt64(truncatingIfNeeded: rmag >> shift)
       }
       let (sum, car) = rhi.addingReportingOverflow(.random(in: 0 ..< bhi))
-      if sum < bhi && !car { return q }
+      return sum < bhi && !car ? q : qAway
       
     case .requireExact:
       preconditionFailure("Division was not exact.")
     }
-    
-    // We didn't have the right result, so round q away from zero.
-    return other.signum() == r.signum() ? q+1 : q-1
   }
   
   // TODO: make this API and make it possible to implement more efficiently.
