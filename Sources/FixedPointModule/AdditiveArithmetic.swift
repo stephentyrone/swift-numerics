f67882aee005b6a13f827aa9bb8f796e5f056b44
Stephen Canon <scanon@apple.com>
2024-08-21 22:00:56 -0400

    Comment formatting fixes.

--- Sources/FixedPointModule/AdditiveArithmetic.swift
+++ Sources/FixedPointModule/AdditiveArithmetic.swift
@@ -57,10 +57,10 @@ extension FixedPoint {
 /// of this number.
 ///
 /// > Note:
-/// > If the type is signed and the argument is ``.min``, then the absolute
+/// > If the type is signed and the argument is `.min`, then the absolute
 /// > value is not representable, and a precondition failure will occur.
 /// >
-/// > To avoid this, you may want to use the ``.magnitude`` property instead,
+/// > To avoid this, you may want to use the `.magnitude` property instead,
 /// > which is always representable.
 /// >
 /// > You could also consider using ``absWithSaturation(_:)``, which returns
@@ -73,11 +73,11 @@ public func abs<T: FixedPoint>(_ a: T) -> T {
 /// The [absolute value](https://en.wikipedia.org/wiki/Absolute_value)
 /// of this number if it is representable, otherwise `.max`.
 ///
-/// If the type is signed and the argument is ``.min``, then ``abs(_:)`` is
+/// If the type is signed and the argument is `.min`, then ``abs(_:)`` is
 /// not representable and a precondition failure occurs.
 ///
 /// The `absWithSaturation(_:)` function avoids this by saturating to `.max`
-/// instead of overflowing. You might also consider using the ``.magnitude``
+/// instead of overflowing. You might also consider using the `.magnitude`
 /// property, which is always representable.
 @inlinable
 public func absWithSaturation<T: FixedPoint>(_ a: T) -> T {
